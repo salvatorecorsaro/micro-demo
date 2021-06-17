2172f4319572f1576e2a94626d59e17059e8f95c
scorsaro <salvatore.corsaro@live.com>
2021-06-17 18:35:04 +0200

    be contained!

--- caffeine-exchange-service/src/main/resources/import.sql
+++ caffeine-exchange-service/src/main/resources/import.sql
@@ -1,11 +1,11 @@
-insert into caffeine_exchange (id, beverage_from, beverage_to, conversion_multiple, environment) values(1,'COFFEE','TEA',65,'');
+insert into caffeine_exchange (id, beverage_from, beverage_to, conversion_multiple, environment) values(1,'COFFEE','TEA',2.81,'');
 
-insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(2,'TEA','COFFEE',65,'');
+insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(2,'TEA','COFFEE',0.27,'');
 
-insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(3,'COFFEE','RED BULL',65,'');
+insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(3,'COFFEE','RED BULL',0.78,'');
 
-insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(4,'RED BULL','COFFEE',65,'');
+insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(4,'RED BULL','COFFEE',1.52,'');
 
-insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(5,'TEA','RED BULL',65,'');
+insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(5,'TEA','RED BULL',0.22,'');
 
-insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(6,'RED BULL','TEA',65,'');
+insert into caffeine_exchange (id,beverage_from,beverage_to,conversion_multiple,environment) values(6,'RED BULL','TEA',4.3,'');
