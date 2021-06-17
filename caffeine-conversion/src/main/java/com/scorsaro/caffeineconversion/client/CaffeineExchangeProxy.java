7ef124d987a9de026779e4d4df911e84a30c177d
scorsaro <salvatore.corsaro@live.com>
2021-06-17 22:33:51 +0200

    kubernetes demo

--- caffeine-conversion/src/main/java/com/scorsaro/caffeineconversion/client/CaffeineExchangeProxy.java
+++ caffeine-conversion/src/main/java/com/scorsaro/caffeineconversion/client/CaffeineExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="caffeine-exchange")
+@FeignClient(name = "caffeine-exchange", url = "${CAFFEINE_EXCHANGE_SERVICE_URI:http://localhost}:8000")
 public interface CaffeineExchangeProxy {
     @GetMapping("/caffeine-exchange/from/{from}/to/{to}")
     CaffeineConversion retrieveExchangeValue( @PathVariable String from, @PathVariable String to);
